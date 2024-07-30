5f463a01be45ae6c50a8d5e81c7b2700a0709d70
Willy <changws12345@gmail.com>
2024-07-30 14:49:58 +0800

    Pass scanner to startGame method caller
    Changes to be committed:
    	modified:   src/oop_assignment/GameOperation.java

--- src/oop_assignment/GameOperation.java
+++ src/oop_assignment/GameOperation.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class GameOperation {
     public static void startGame(Scanner scanner, Player player, ScoreManager scoreManager) {
         Game game = new Game(player, scoreManager);
-        game.startGame();
+        game.startGame(scanner);
     }
 }
